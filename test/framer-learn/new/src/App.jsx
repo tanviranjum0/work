cf1da8f1c1e1b82ece845ab86134c371fb3d1739
Tanvir Anjum <tanviranjum010@gmail.com>
2025-04-11 00:04:14 +0600

    hola

--- test/framer-learn/new/src/App.jsx
+++ test/framer-learn/new/src/App.jsx
@@ -12,28 +12,7 @@ const App = () => {
   }
   return (
     <div className='flex justify-center items-center bg-gray-700 h-[100vh] w-[100vw] text-4xl'>
-      {/* <motion.div 
-        whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        style={{ originX: 0.5 }}
-        transition={{ duration: 0.4, ease: 'easeInOut' }}
-      >Hello</motion.div> */}
-      <motion.div
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 180, -180, 0],
-          borderRadius: ["0%", "0%", "50%", "50%", "0%"],
-        }}
-        transition={{
-          duration: 2,
-          ease: "linear",
-          times: [0, 2, 5, 8, 10],
-          repeat: Infinity,
-          repeatDelay: 1,
-        }}
-        style={box}
-      />
+      Hello
     </div >
   )
 }
