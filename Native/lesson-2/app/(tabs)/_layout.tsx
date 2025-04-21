ced9c503729b6b8ea9aefa08a3542efb70d74788
Tanvir Anjum <tanviranjum010@gmail.com>
2025-04-22 00:13:20 +0600

    holla

---
+++ Native/lesson-2/app/(tabs)/_layout.tsx
@@ -0,0 +1,54 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        // tabBarInActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        headerTitleAlign: "center",
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: "Contact us",
+          tabBarIcon: ({ color, focused }) => (
+            <IconSymbol
+              size={28}
+              name={focused ? "people.fill" : "people-outline.fill"}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ Native/lesson-2/app/(tabs)/contact.jsx
@@ -0,0 +1,68 @@
+import { View, Text, StyleSheet, ImageBackground, Pressable } from "react-native";
+import React from "react";
+import demoBg from "../../assets/images/partial-react-logo.png";
+import { Link } from "expo-router";
+const app = () => {
+    return (
+        <View style={styles.container}>
+            <ImageBackground style={styles.image} source={demoBg}>
+                <Text style={styles.text}>Coffee Shop</Text>
+                <Link Pressabl asChild href="/contact" style={{ marginHorizontal: "auto" }}>
+                    <Pressable style={styles.button}>
+                        <Text style={styles.buttonText}>Contact us</Text>
+                    </Pressable>
+                </Link>
+            </ImageBackground>
+        </View >
+    );
+};
+
+export default app;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    image: {
+        resizeMode: "cover",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        color: "white",
+        width: "100%",
+        fontSize: 42,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: 20,
+    },
+    link: {
+        color: "white",
+        width: "100%",
+        fontSize: 42,
+        textDecorationLine: "underline",
+        padding: 4,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    button: {
+        height: 60,
+        justifyContent: "center",
+        color: "white",
+        borderRadius: 10,
+        backgroundColor: "rgba(0,0,0,0.75)",
+        padding: 6,
+    },
+    buttonText: {
+        color: "white",
+        width: "100%",
+        fontSize: 16,
+        padding: 4,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
