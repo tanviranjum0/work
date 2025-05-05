01e5400efe3a7246144fb7bf292fe3e7c59a8f3b
Tanvir Anjum <tanviranjum010@gmail.com>
2025-05-05 16:55:13 +0600

    holla

--- test/rideTest/src/App.jsx
+++ test/rideTest/src/App.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, useMap, APIProviderContext, useMarkerRef, Marker } from '@vis.gl/react-google-maps';
 
 const App = () => {
-  console.log(import.meta.env.VITE_MAP_API);
+  const [markerRef, marker] = useMarkerRef();
+
+  React.useEffect(() => {
+    if (!marker) {
+      return;
+    }
+    console.log('marker', marker);
+    // do something with marker instance here
+  }, [marker]);
+
+  const MyComponent = () => {
+    const map = useMap();
+
+    React.useEffect(() => {
+      if (!map) return;
+      console.log('map', map);
+      // here you can interact with the imperative maps API
+    }, [map]);
+  };
   return (
-    < APIProvider apiKey={import.meta.env.VITE_MAP_API} onLoad={() => console.log('Maps API has loaded.')}>
+    <APIProvider apiKey={import.meta.env.VITE_MAP_API}>
       <Map
         style={{ width: '100vw', height: '100vh' }}
         mapOptions={{
-          disableDefaultUI: true,
           zoomControl: true,
           streetViewControl: false,
           fullscreenControl: false,
           mapTypeControl: false,
         }}
-        onLoad={() => console.log('Map has loaded.')}
-        onUnmount={() => console.log('Map has unmounted.')}
-        onClick={() => console.log('Map has been clicked.')}
-        onZoomChanged={() => console.log('Map zoom has changed.')}
-        onBoundsChanged={() => console.log('Map bounds have changed.')}
-        defaultZoom={13}
-        defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+        defaultZoom={8}
+        defaultCenter={{ lat: 22.3752, lng: 91.8349 }}
       >
+
+        <Marker ref={markerRef} position={{ lat: 22.3752, lng: 91.8349 }} />
       </Map>
-    </APIProvider >
+      <MyComponent />
+    </APIProvider>
   )
 }
 
