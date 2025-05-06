42d4cdfdc7485733aa55427accb984f7ad2cfe93
Tanvir Anjum <tanviranjum010@gmail.com>
2025-05-06 16:10:00 +0600

    hola

--- test/rideTest/src/App.jsx
+++ test/rideTest/src/App.jsx
@@ -1,47 +1,131 @@
-import React from 'react';
-import { APIProvider, Map, useMap, APIProviderContext, useMarkerRef, Marker } from '@vis.gl/react-google-maps';
+// import React from 'react';
+// import { APIProvider, Map, useMap, useMapsLibrary, APIProviderContext, useMarkerRef, Marker } from '@vis.gl/react-google-maps';
+
+// const App = () => {
+//   const MyComponent1 = () => {
+//     const map = useMap();
+
+//     // triggers loading the places library and returns the API Object once complete (the
+//     // component calling the hook gets automatically re-rendered when this is
+//     // the case)
+//     const placesLibrary = useMapsLibrary('places');
+
+//     const [placesService, setPlacesService] = React.useState(null);
+
+//     React.useEffect(() => {
+//       if (!placesLibrary || !map) return;
+//       console.log('placesLibrary', placesLibrary);
+//       console.log('map', map);
+//       // when placesLibrary is loaded, the library can be accessed via the
+//       // placesLibrary API object
+//       setPlacesService(new placesLibrary.PlacesService(map));
+//     }, [placesLibrary, map]);
+
+//     React.useEffect(() => {
+//       if (!placesService) return;
+//       console.log('placesService', placesService);
+//       // ...use placesService...
+//     }, [placesService]);
+
+//     return <></>;
+//   };
+//   const [markerRef, marker] = useMarkerRef();
+
+//   React.useEffect(() => {
+//     if (!marker) {
+//       return;
+//     }
+//     // console.log('marker', marker);
+//     // do something with marker instance here
+//   }, [marker]);
+
+//   const MyComponent = () => {
+//     const map = useMap();
+
+//     React.useEffect(() => {
+//       if (!map) return;
+//       // console.log('map', map);
+//       // here you can interact with the imperative maps API
+//     }, [map]);
+//   };
+//   return (
+//     <APIProvider apiKey={import.meta.env.VITE_MAP_API}>
+//       <Map
+//         style={{ width: '100vw', height: '100vh' }}
+//         mapOptions={{
+//           zoomControl: true,
+//           streetViewControl: false,
+//           fullscreenControl: false,
+//           mapTypeControl: false,
+//         }}
+//         defaultZoom={8}
+//         defaultCenter={{ lat: 22.3752, lng: 91.8349 }}
+//       >
+//         <Marker
+//           ref={markerRef}
+//           position={{ lat: 22.3752, lng: 91.8349 }} />
+//       </Map>
+//       <MyComponent />
+//       <MyComponent1 />
+//     </APIProvider>
+//   )
+// }
+
+
+
+// export default App;
+
+
+// import { useEffect, useMemo } from 'react';
+// import { useMap, APIProvider } from '@vis.gl/react-google-maps';
+// import { GoogleMapsOverlay } from '@deck.gl/google-maps';
+
+// const DeckGlOverlay = ({ layers }) => {
+//   const deck = useMemo(() => new GoogleMapsOverlay({ interleaved: true }), []);
+
+//   const map = useMap();
+//   useEffect(() => {
+//     deck.setMap(map);
+
+//     return () => deck.setMap(null);
+//   }, [map]);
+//   useEffect(() => deck.setProps({ layers }), [layers]);
+
+//   // no dom rendered by this component
+//   return null;
+// };
+// const mapProps = {
+//   zoomControl: true,
+//   streetViewControl: false,
+//   fullscreenControl: false,
+//   mapTypeControl: false,
+// }
+// const App = () => {
+//   const deckGlLayers = useMemo(() => {
+//     // Define your layers here
+//     return [];
+//   }, []);
+
+//   return (
+//     <APIProvider apiKey={import.meta.env.VITE_MAP_API}>
+//       <Map {...mapProps}>
+//         <DeckGlOverlay layers={deckGlLayers} />
+//       </Map>
+//     </APIProvider>
+//   );
+// };
+// export default DeckGlOverlay
+
+
+import React from 'react'
+import Test from './components/Test'
 
 const App = () => {
-  const [markerRef, marker] = useMarkerRef();
-
-  React.useEffect(() => {
-    if (!marker) {
-      return;
-    }
-    console.log('marker', marker);
-    // do something with marker instance here
-  }, [marker]);
-
-  const MyComponent = () => {
-    const map = useMap();
-
-    React.useEffect(() => {
-      if (!map) return;
-      console.log('map', map);
-      // here you can interact with the imperative maps API
-    }, [map]);
-  };
   return (
-    <APIProvider apiKey={import.meta.env.VITE_MAP_API}>
-      <Map
-        style={{ width: '100vw', height: '100vh' }}
-        mapOptions={{
-          zoomControl: true,
-          streetViewControl: false,
-          fullscreenControl: false,
-          mapTypeControl: false,
-        }}
-        defaultZoom={8}
-        defaultCenter={{ lat: 22.3752, lng: 91.8349 }}
-      >
-
-        <Marker ref={markerRef} position={{ lat: 22.3752, lng: 91.8349 }} />
-      </Map>
-      <MyComponent />
-    </APIProvider>
+    <div>
+      <Test />
+    </div>
   )
 }
 
-
-
-export default App;
+export default App
---
+++ test/rideTest/src/components/Test.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import {
+    APIProvider,
+    Map,
+    useMapsLibrary,
+    useMap
+} from '@vis.gl/react-google-maps';
+// import ControlPanel from './control-panel';
+
+const API_KEY = import.meta.env.VITE_MAP_API;
+const App = () => (
+    <APIProvider apiKey={API_KEY}>
+        <Map
+            defaultCenter={{ lat: 43.65, lng: -79.38 }}
+            defaultZoom={9}
+            gestureHandling={'greedy'}
+            fullscreenControl={false}>
+            <Directions />
+        </Map>
+        {/* <ControlPanel /> */}
+    </APIProvider>
+);
+
+function Directions() {
+    const map = useMap();
+    const routesLibrary = useMapsLibrary('routes');
+    const [directionsService, setDirectionsService] = useState();
+    const [directionsRenderer, setDirectionsRenderer] = useState();
+    const [routes, setRoutes] = useState([]);
+    const [routeIndex, setRouteIndex] = useState(0);
+    const selected = routes[routeIndex];
+    const leg = selected?.legs[0];
+
+    // Initialize directions service and renderer
+    useEffect(() => {
+        if (!routesLibrary || !map) return;
+        setDirectionsService(new routesLibrary.DirectionsService());
+        setDirectionsRenderer(
+            new routesLibrary.DirectionsRenderer({
+                draggable: true, // Only necessary for draggable markers
+                map
+            })
+        );
+    }, [routesLibrary, map]);
+
+    // Add the following useEffect to make markers draggable
+    useEffect(() => {
+        if (!directionsRenderer) return;
+
+        // Add the listener to update routes when directions change
+        const listener = directionsRenderer.addListener(
+            'directions_changed',
+            () => {
+                const result = directionsRenderer.getDirections();
+                if (result) {
+                    setRoutes(result.routes);
+                }
+            }
+        );
+
+        return () => window.google.maps.event.removeListener(listener);
+    }, [directionsRenderer]);
+
+    // Use directions service
+    useEffect(() => {
+        if (!directionsService || !directionsRenderer) return;
+
+        directionsService
+            .route({
+                origin: '100 Front St, Toronto ON',
+                destination: '500 College St, Toronto ON',
+                travelMode: window.google.maps.TravelMode.DRIVING,
+                provideRouteAlternatives: true
+            })
+            .then(response => {
+                directionsRenderer.setDirections(response);
+                setRoutes(response.routes);
+            });
+
+        return () => directionsRenderer.setMap(null);
+    }, [directionsService, directionsRenderer]);
+
+    // Update direction route
+    useEffect(() => {
+        if (!directionsRenderer) return;
+        directionsRenderer.setRouteIndex(routeIndex);
+    }, [routeIndex, directionsRenderer]);
+
+    if (!leg) return null;
+
+    return (
+        <div className="directions">
+            <h2>{selected.summary}</h2>
+            <p>
+                {leg.start_address.split(',')[0]} to {leg.end_address.split(',')[0]}
+            </p>
+            <p>Distance: {leg.distance?.text}</p>
+            <p>Duration: {leg.duration?.text}</p>
+
+            <h2>Other Routes</h2>
+            <ul>
+                {routes.map((route, index) => (
+                    <li key={route.summary}>
+                        <button onClick={() => setRouteIndex(index)}>
+                            {route.summary}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
