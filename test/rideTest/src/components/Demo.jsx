2c77c2505f3f8f1ca52349d13f41faa30af26020
Tanvir Anjum <tanviranjum010@gmail.com>
2025-06-02 00:53:15 +0600

    holl

--- test/rideTest/src/components/Demo.jsx
+++ test/rideTest/src/components/Demo.jsx
@@ -8,14 +8,17 @@ const App = () => {
         <APIProvider apiKey={import.meta.env.VITE_MAP_API} >
             <div style={{ width: '100vw', height: '100vh' }}>
                 <Map
-                    // mapId={import.meta.env.VITE_MAP_ID}
                     zoom={8}
+                    mapId={import.meta.env.VITE_MAP_ID}
                     center={{ lat: 37.7749, lng: -122.4194 }} // San Francisco
-                ><AdvancedMarker onClick={() => setOpen(true)} position={{ lat: 37.7749, lng: -122.4194 }}><Pin background={"gray"} borderColor={"green"} glyphColor={"purple"} /></AdvancedMarker>
-                    {open && <InfoWindow onCloseClick={() => setOpen(false)} position={{ lat: 37.7749, lng: -122.4194 }}><div>I am in san fransisco</div></InfoWindow>}
+                >
+                    <AdvancedMarker onClick={() => setOpen(true)} position={{ lat: 37.7749, lng: -122.4194 }}><Pin background={"violet"} borderColor={"green"} glyphColor={"purple"} />
+                    </AdvancedMarker>
+                    {open && <InfoWindow onCloseClick={() => setOpen(false)} position={{ lat: 37.7749, lng: -122.4194 }}><div className='bg-gray-200 font-bold text-xl border-2 p-5'> I am in san fransisco</div></InfoWindow>}
                 </Map>
             </div>
         </APIProvider>
     );
 }
 export default App;
+
