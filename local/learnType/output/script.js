6101bc8d0b48611c96e77737e8d09e2fc1c5f646
Tanvir Anjum <tanviranjum010@gmail.com>
2025-06-25 14:09:04 +0600

    holla

---
+++ local/learnType/output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+var a = [];
+a.push("string");
+a.push(57);
+console.log(a);
+var c;
+c = ["Helo"];
---
+++ local/learnType/src/script.ts
@@ -0,0 +1,8 @@
+let a: (number | string)[] = [];
+a.push("string");
+
+a.push(57);
+console.log(a);
+
+let c: object;
+c = ["Helo"];
