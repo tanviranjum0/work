2ef487bc0923ab19986c4ef2b0b5eb064e0a9378
Tanvir Anjum <tanviranjum010@gmail.com>
2025-07-23 02:21:54 +0600

    holla

--- test/tailwindV4/src/App.jsx
+++ test/tailwindV4/src/App.jsx
@@ -1,8 +1,12 @@
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-4xl">
-      Helo
+    <div>
+      {/* <div className="text-4xl mx-auto">Hello World</div> */}
+      <label htmlFor="" className='my-4 block'>
+        <input type="file" name="file" id="file" className='block w-full text-sm text-slate-500 file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100' />
+      </label>
     </div>
   )
 }
