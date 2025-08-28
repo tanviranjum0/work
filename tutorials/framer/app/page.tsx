2b0555b118c96f10d8ad98a70bf9147218e752f3
Tanvir Anjum <tanviranjum010@gmail.com>
2025-08-29 01:27:10 +0600

    holla

--- tutorials/framer/app/page.tsx
+++ tutorials/framer/app/page.tsx
@@ -1,23 +1,11 @@
-import React from "react";
 import Test1 from "@/components/Basics/Test1";
 import Test2 from "@/components/Basics/Test2";
-import Day3 from "@/components/buttons/Day3";
-import Day4 from "@/components/buttons/Day4";
-import Day6 from "@/components/buttons/Day6";
-import Day7 from "@/components/buttons/Day7";
-import Day8 from "@/components/buttons/Day8";
-import Day9 from "@/components/buttons/Day9";
-import Day10 from "@/components/buttons/Day10";
-import Day11 from "@/components/buttons/Day11";
 import Test3 from "@/components/Basics/Test3";
-import UseInView from "@/components/Basics/hooks/UseInView";
-import UseAnimate from "@/components/Basics/hooks/UseAnimate";
-import UseAnimate2 from "@/components/Basics/hooks/UseAnimate2";
 const page = () => {
   return (
     <div className="">
-      <Test1 />
-      {/* <Test2 /> */}
+      {/* <Test1 /> */}
+      <Test2 />
       {/* <Test3 /> */}
     </div>
   );
