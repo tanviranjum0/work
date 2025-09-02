09a554509e0e9f56056db6693aeca3f596551d2e
Tanvir Anjum <tanviranjum010@gmail.com>
2025-09-03 01:05:10 +0600

    hola

--- tutorials/framer/components/Basics/Test2.tsx
+++ tutorials/framer/components/Basics/Test2.tsx
@@ -1,33 +1,53 @@
 "use client";
-import { motion } from "framer-motion";
-
-export default function MarqueeText() {
-  const texts = [
-    "Frontend",
-    "Backend",
-    "Fullstack",
-    "JavaScript",
-    "React",
-    "Node.js",
-  ];
-
+import Marquee from "react-fast-marquee";
+import { motion } from "motion/react";
+const Test2 = () => {
+  const handleHoverStart = (event) => {
+    const target = document.getElementsByClassName(
+      "marquee"
+    ) as HTMLCollectionOf<Element>;
+    target[1].classList.add("italic", "text-4xl", "rotate-30");
+    target[0].classList.add("italic", "text-4xl");
+  };
+  const handleHoverEnd = (event) => {
+    const target = document.getElementsByClassName(
+      "marquee"
+    ) as HTMLCollectionOf<Element>;
+    target[1].classList.remove("italic", "text-4xl");
+    target[0].classList.remove("italic", "text-4xl");
+  };
+  const textsMain = ["React", "Node.js", "Framer Motion", "Tailwind"];
   return (
-    <div className="relative overflow-hidden w-full h-20 bg-gray-700 flex items-center">
-      <motion.div
-        className="flex gap-20  text-3xl font-bold text-white whitespace-nowrap"
-        animate={{ x: ["100%", "-100%"] }}
-        transition={{
-          repeat: Infinity,
-          duration: 5,
-          repeatType: "loop",
-          ease: "linear",
-        }}
+    <motion.div onHoverStart={handleHoverStart} onHoverEnd={handleHoverEnd}>
+      <Marquee
+        direction="right"
+        className="text-3xl marquee py-4 bg-sky-300"
+        autoFill={true}
+        speed={50}
       >
-        {/* Duplicate text sequence for seamless looping */}
-        {[...texts, ...texts].map((text, i) => (
-          <span key={i}>{text}</span>
-        ))}
-      </motion.div>
-    </div>
+        {textsMain.map((text, index) => {
+          return (
+            <span key={index} className="mx-8 text-xl font-semibold">
+              {text}
+            </span>
+          );
+        })}
+      </Marquee>{" "}
+      <Marquee
+        className="text-3xl marquee py-4 bg-amber-200"
+        autoFill={true}
+        speed={50}
+      >
+        {textsMain.map((text, index) => {
+          return (
+            <span key={index} className="mx-8 text-xl font-semibold">
+              {text}
+            </span>
+          );
+        })}
+      </Marquee>
+    </motion.div>
   );
-}
+};
+
+export default Test2;
