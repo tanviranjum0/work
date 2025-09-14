2e6ab688523d61cca22d46bb229b0e00a650f702
Tanvir Anjum <tanviranjum010@gmail.com>
2025-09-15 01:54:01 +0600

    ola

--- tutorials/framer/components/Gigapp/components/PseudoScroll.tsx
+++ tutorials/framer/components/Gigapp/components/PseudoScroll.tsx
@@ -1,6 +1,11 @@
 "use client";
 import React from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "motion/react";
 
 export default function StickyScrollGallery() {
   const containerRef = React.useRef<HTMLDivElement>(null);
@@ -8,34 +13,23 @@ export default function StickyScrollGallery() {
     target: containerRef,
     offset: ["start start", "end end"], // maps full section scroll
   });
-
+  // console.log(scrollYProgress);
   // Transform scroll progress (0 → 1) into vertical translation
   const y = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
-
+  useMotionValueEvent(y, "change", (latest) => {
+    console.log("Latest Value", latest);
+  });
   return (
     <div
       ref={containerRef}
       className="relative flex w-full min-h-[200vh] bg-gradient-to-br from-gray-100 to-gray-200"
     >
       {/* Left sticky side */}
-      <div className="w-1/2 sticky top-0 h-screen flex items-center justify-center p-8">
-        <motion.div
-          className="text-4xl font-bold text-gray-900"
-          style={{ y: useTransform(scrollYProgress, [0, 1], ["0%", "-10%"]) }}
-        >
-          Explore Stunning Visuals 🌍
-        </motion.div>
-      </div>
-
-      {/* Right scrollable side */}
-      <div className="w-1/2 relative overflow-hidden">
+      <div className="w-2/4 relative overflow-hidden">
         <motion.div style={{ y }} className="flex flex-col gap-8 py-20 px-8">
           {[
-            "https://picsum.photos/id/1015/600/400",
-            "https://picsum.photos/id/1016/600/400",
-            "https://picsum.photos/id/1018/600/400",
-            "https://picsum.photos/id/1019/600/400",
-            "https://picsum.photos/id/1020/600/400",
+            "https://framerusercontent.com/images/NYXSuNHIqB6GMw7IXSDSqYLr8FM.gif",
+            "https://framerusercontent.com/images/YTx0Nilb6NZn0z5EnXtsE4s78.gif",
           ].map((src, i) => (
             <motion.img
               key={i}
@@ -48,6 +42,16 @@ export default function StickyScrollGallery() {
           ))}
         </motion.div>
       </div>
+      <div className="w-2/4 sticky top-0 h-screen flex items-center justify-center p-8">
+        <motion.div
+          className="text-4xl font-bold text-gray-900"
+          style={{ y: useTransform(scrollYProgress, [0, 1], ["0%", "-10%"]) }}
+        >
+          Explore Stunning Visuals 🌍
+        </motion.div>
+      </div>
+
+      {/* Right scrollable side */}
     </div>
   );
 }
