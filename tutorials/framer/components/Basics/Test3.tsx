e0b879bb8240a4a928a46adac3087c7855c64329
Tanvir Anjum <tanviranjum010@gmail.com>
2025-10-03 01:08:27 +0600

    holla

--- tutorials/framer/components/Basics/Test3.tsx
+++ tutorials/framer/components/Basics/Test3.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { DualRangeSlider } from "./Test5";
 
 const Test3 = () => {
+  const [leftSection, setLeftSection] = useState(false);
+  const [db, setDb] = useState({
+    bounce: 0,
+    duration: 0,
+  });
+  const [sdm, setSdm] = useState({
+    stiffness: 0,
+    mass: 0,
+    damping: 0,
+    velocity: 0,
+    restDelta: 0,
+    restSpeed: 0,
+  });
+  const [widthPercentage, setWidthPercentage] = useState(25);
+  useEffect(() => {
+    console.log(widthPercentage);
+  }, [widthPercentage]);
   return (
     <div className="h-[100vh] p-10 bg-conic from-blue-600 to-sky-400 to-50%">
       <div className="text-4xl my-3">Spring Setting</div>
@@ -15,13 +34,151 @@ const Test3 = () => {
         <div className="bg-gray-600 p-[10px] h-[10rem] col-span-2 rounded-xl"></div>
       </div>
       <div className="grid grid-cols-2 gap-5">
-        <div className="">
+        <div onClick={() => setLeftSection(true)} className="">
           <div className="text-3xl my-2">Duration and Bounce</div>
-          <div className="border-2 rounded-2xl"></div>
+          <div className="border-2 p-3 rounded-2xl">
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Duration</span>
+              <div className="flex gap-4 col-span-10 ml-1">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[db.duration]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={3}
+                  step={0.03}
+                />
+              </div>
+            </div>
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Bounce</span>
+              <div className="flex gap-4 col-span-10 ml-1">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[db.bounce]}
+                  onValueChange={([bounce]) =>
+                    bounce != null &&
+                    setDb((prev)=>{...prev})
+                  }
+                  min={0}
+                  max={1}
+                  step={0.01}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="">
+        <div onClick={() => setLeftSection(false)} className="">
           <div className="text-3xl my-2">Stiffness, Damping, Mass...</div>
-          <div className="border-2 rounded-2xl"></div>
+          <div className="border-2 p-3 rounded-2xl">
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Stiffness</span>
+              <div className="flex gap-4 col-span-10 ml-2">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[widthPercentage]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={300}
+                  step={3}
+                />
+              </div>
+            </div>
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Damping</span>
+              <div className="flex gap-4 col-span-10 ml-2">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[widthPercentage]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={100}
+                  step={1}
+                />
+              </div>
+            </div>
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Mass</span>
+              <div className="flex gap-4 col-span-10 ml-2">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[widthPercentage]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={10}
+                  step={0.1}
+                />
+              </div>
+            </div>
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Velocity</span>
+              <div className="flex gap-4 col-span-10 ml-2">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[widthPercentage]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={50}
+                  step={1}
+                />
+              </div>
+            </div>
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Rest Delta</span>
+              <div className="flex gap-4 col-span-10 ml-2">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[widthPercentage]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={1}
+                  step={0.01}
+                />
+              </div>
+            </div>
+            <div className="mx-5 grid grid-cols-12 py-1.5">
+              <span className="col-span-2 font-semibold">Rest Speed</span>
+              <div className="flex gap-4 col-span-10 ml-2">
+                <DualRangeSlider
+                  label
+                  lableContenPos={"left"}
+                  value={[widthPercentage]}
+                  onValueChange={([widthPercentage]) =>
+                    widthPercentage != null &&
+                    setWidthPercentage(widthPercentage)
+                  }
+                  min={0}
+                  max={1}
+                  step={0.01}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tutorials/framer/components/Basics/Test4.tsx
+++ tutorials/framer/components/Basics/Test4.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DualRangeSlider } from "./Test5";
 
 export default function index() {
-  const [widthPercentage, setWidthPercentage] = useState(70);
+  const [widthPercentage, setWidthPercentage] = useState(25);
+  useEffect(() => {
+    console.log(widthPercentage);
+  }, [widthPercentage]);
   return (
     <>
       <div className="w-3/5 mx-auto  px-10 gap-10 py-16">
@@ -17,7 +20,7 @@ export default function index() {
               widthPercentage != null && setWidthPercentage(widthPercentage)
             }
             min={0}
-            max={100}
+            max={50}
             step={1}
           />
         </div>
