6340619316ca8b6c2421ce492e38f1f922f257b0
Tanvir Anjum <tanviranjum010@gmail.com>
2025-10-14 00:57:11 +0600

    holla

--- tutorials/framer/components/Gigapp/components/Footer.tsx
+++ tutorials/framer/components/Gigapp/components/Footer.tsx
@@ -101,7 +101,7 @@ const Footer = () => {
                   htmlFor="message"
                   className="peer-focus:font-medium absolute text-sm   duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-gray-200 peer-focus:dark:text-gray-200 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Message
+                  {/* Message */}
                 </label>
               </div>
               <div className="">
