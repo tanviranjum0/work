6b0d1fe65d0f535f52c9b18909bee044b26f7195
Tanvir Anjum <tanviranjum010@gmail.com>
2025-10-22 00:35:29 +0600

    holla

--- local/framer-app/Gigapp/components/Footer.tsx
+++ local/framer-app/Gigapp/components/Footer.tsx
@@ -216,7 +216,7 @@ const Footer = () => {
                       htmlFor="name"
                       className="peer-focus:font-medium absolute text-sm   duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-gray-200 peer-focus:dark:text-gray-200 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                     >
-                      {/* Full Name */}
+                      Full Name
                     </label>
                   </div>
                   <div className="relative z-0 w-full group">
