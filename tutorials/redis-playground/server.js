3f288856112865e354b8e08e8aea0bcb18843508
Tanvir Anjum <tanviranjum010@gmail.com>
2025-11-27 15:20:47 +0600

    holla

--- tutorials/redis-playground/server.js
+++ tutorials/redis-playground/server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const axios = require("axios");
-const client = require("./client");
+// const express = require("express");
+// const axios = require("axios");
+// const client = require("./client");
 
-const app = express();
-app.get("/", async (req, res) => {
-  const cachedValue = await client.get("todos");
-  if (cachedValue) return res.json(cachedValue);
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/todos"
-  );
-  await client.set("todos", JSON.stringify(data));
-  await client.expire("todos", 5);
-  return res.json(data);
-});
+// const app = express();
+// app.get("/", async (req, res) => {
+//   const cachedValue = await client.get("todos");
+//   if (cachedValue) return res.json(cachedValue);
+//   const { data } = await axios.get(
+//     "https://jsonplaceholder.typicode.com/todos"
+//   );
+//   await client.set("todos", JSON.stringify(data));
+//   await client.expire("todos", 5);
+//   return res.json(data);
+// });
 
-app.listen(3000, () => {
-  console.log("server", 3000);
-});
+// app.listen(3000, () => {
+//   console.log("server", 3000);
+// });
--- tutorials/redis-playground/string.js
+++ tutorials/redis-playground/string.js
@@ -1,8 +1,8 @@
 const client = require("./client");
 async function init() {
-  // await client.set("message:4", "new msg");
-  // await client.expire("message:4", 10);
-  const result = await client.get("message:4");
+  // await client.set("message:5", "new msg");
+  // await client.expire("message:5", 10);
+  const result = await client.get("message:5");
 
   console.log("Result", result);
 }
