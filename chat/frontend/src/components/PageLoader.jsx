0c9a4c73d37a9fa9c286534f207c614878544ad6
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-08 22:15:49 +0600

    holla

--- chat/frontend/src/components/PageLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { LoaderIcon } from "lucide-react";
-function PageLoader() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <LoaderIcon className="size-10 animate-spin" />
-    </div>
-  );
-}
-export default PageLoader;
