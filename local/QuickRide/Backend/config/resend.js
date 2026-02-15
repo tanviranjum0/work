4f5342e23887006ade23869c6a8fd270fde070b8
Tanvir <tanviranjum010@gmail.com>
2026-02-16 00:52:06 +0600

    holla

--- local/QuickRide/Backend/config/resend.js
+++ local/QuickRide/Backend/config/resend.js
@@ -1,4 +1,5 @@
 import { Resend } from "resend";
+
 export const resendClient = new Resend(process.env.RESEND_API);
 
 export const sender = {
--- local/QuickRide/Backend/controllers/mail.controller.js
+++ local/QuickRide/Backend/controllers/mail.controller.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 
 const { sendMail } = require("../services/mail.service");
 let { fillTemplate } = require("../templates/mail.template");
-
 const captainModel = require("../models/captain.model");
 const userModel = require("../models/user.model");
 
@@ -21,11 +20,17 @@ module.exports.sendVerificationEmail = asyncHandler(async (req, res) => {
   } else if (req.userType === "captain") {
     user = req.captain;
   } else {
-    return res.status(400).json({ message: "The email verification link is invalid because of incorrect user type" });
+    return res.status(400).json({
+      message:
+        "The email verification link is invalid because of incorrect user type",
+    });
   }
 
   if (user.emailVerified) {
-    return res.status(400).json({ message: "Your email is already verified. You may continue using the application." });
+    return res.status(400).json({
+      message:
+        "Your email is already verified. You may continue using the application.",
+    });
   }
 
   const token = jwt.sign(
@@ -33,13 +38,14 @@ module.exports.sendVerificationEmail = asyncHandler(async (req, res) => {
     process.env.JWT_SECRET,
     {
       expiresIn: "15m",
-    }
+    },
   );
 
   if (!token) {
-    return res
-      .status(500)
-      .json({ message: "We're unable to generate a verification link at the moment. Please try again shortly." });
+    return res.status(500).json({
+      message:
+        "We're unable to generate a verification link at the moment. Please try again shortly.",
+    });
   }
 
   try {
@@ -48,7 +54,8 @@ module.exports.sendVerificationEmail = asyncHandler(async (req, res) => {
     let mailHtml = fillTemplate({
       title: "Email Verification Required",
       name: user.fullname.firstname,
-      message: "Thank you for signing up with QuickRide! To complete your registration and activate your account, please verify your email address by clicking the button below.",
+      message:
+        "Thank you for signing up with QuickRide! To complete your registration and activate your account, please verify your email address by clicking the button below.",
       cta_link: verification_link,
       cta_text: "Verify Email",
       note: "For your security, this verification link is valid for only <strong>15 minutes</strong>.  If the link expires, you can request a new one from the login page. <br/>If you did not create a QuickRide account, please disregard this email.",
@@ -57,7 +64,7 @@ module.exports.sendVerificationEmail = asyncHandler(async (req, res) => {
     const result = await sendMail(
       user.email,
       "QuickRide - Email Verification",
-      mailHtml
+      mailHtml,
     );
 
     return res.status(200).json({
@@ -90,12 +97,15 @@ module.exports.forgotPassword = asyncHandler(async (req, res) => {
   } else if (userType === "captain") {
     user = await captainModel.findOne({ email });
   }
-  if (!user) return res.status(404).json({ message: "User not found. Please check your credentials and try again" });
+  if (!user)
+    return res.status(404).json({
+      message: "User not found. Please check your credentials and try again",
+    });
 
   const token = jwt.sign(
     { id: user._id, type: "user" },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: "15m" },
   );
 
   const resetLink = `${process.env.CLIENT_URL}/${userType}/reset-password?token=${token}`;
@@ -103,7 +113,8 @@ module.exports.forgotPassword = asyncHandler(async (req, res) => {
   let mailHtml = fillTemplate({
     title: "Reset Password",
     name: user.fullname.firstname,
-    message: "We received a request to reset the password associated with your QuickRide account. If you made this request, please click the button below to proceed.",
+    message:
+      "We received a request to reset the password associated with your QuickRide account. If you made this request, please click the button below to proceed.",
     cta_link: resetLink,
     cta_text: "Reset Password",
     note: "If you didn’t request a password reset, you can safely ignore this email. Your current password will remain unchanged. <br/>This verification link is valid for <strong>15 minutes</strong> only.",
