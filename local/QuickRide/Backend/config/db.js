a28de8157085cd75af40292f2190bedfc6c78fb4
Tanvir <tanviranjum010@gmail.com>
2026-03-04 19:39:59 +0600

    holla

--- local/QuickRide/Backend/config/db.js
+++ local/QuickRide/Backend/config/db.js
@@ -6,14 +6,13 @@ let MONGO_DB = {
 };
 
 let environment = process.env.ENVIRONMENT;
-
 mongoose
   .connect(MONGO_DB[environment].url)
   .then(() => {
     console.log("Connected to Mongo DB", MONGO_DB[environment].type);
   })
-  .catch(() => {
-    console.log("Failed to connect to MongoDB");
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
   });
 
 module.exports = mongoose.connection;
