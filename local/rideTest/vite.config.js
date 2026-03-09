fd5289b68234aa5d70f318154323de4f80a46d6f
Tanvir <tanviranjum010@gmail.com>
2026-03-10 01:25:09 +0600

    holla

--- local/rideTest/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({
-  server: {
-    allowedHosts: true,
-  },
-  plugins: [tailwindcss(), react()],
-});
