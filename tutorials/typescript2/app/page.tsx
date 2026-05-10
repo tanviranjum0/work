b7d741d49cb25bb6cfdcc53335b94ad1977dfd0d
Tanvir Anjum <tanviranjum010@gmail.com>
2026-05-10 23:43:49 +0600

    Fix comment formatting in page.tsx

--- tutorials/typescript2/app/page.tsx
+++ tutorials/typescript2/app/page.tsx
@@ -2,7 +2,7 @@ const page = () => {
   return (
     <div className="flex justify-center h-screen items-center">
       <div className="text-xl cursor-pointer p-5 border bg-gray-300 hover:bg-gray-200">
-        {/* Button */}
+        {/* Button*/}
       </div>
     </div>
   );
